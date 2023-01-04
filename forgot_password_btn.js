47004d2d6e119a232cfcbf56c37247ca87854d93
Yaryna-Martyniuk <yaruna2904@gmail.com>
2023-01-04 12:05:58 +0000

    link to restore passwird page changed

--- forgot_password_btn.js
+++
@@ -1,7 +0,0 @@
-
-function forgotPass(){
-    if(document.querySelector(".forgot_password").checked = true){
-        document.querySelector(".btn btn-outline-primary").replaceWith('Send new password')
-    }
-}
-forgotPass();
